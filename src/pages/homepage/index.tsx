9c6f51c13d9c716ebbe2bb6d75e301cb8208f43c
Ari Nugraha <nugrahaa878@gmail.com>
2025-02-18 16:55:11 +0700

    add section service

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,6 +1,7 @@
 import Navbar from '../../components/navbar';
 import HeroSection from '../../components/hero-section';
 import PackageHighlights from 'components/package-highlights';
+import ServicesOverview from 'components/services-overview';
 
 function HomePage() {
   return (
@@ -8,6 +9,7 @@ function HomePage() {
       <Navbar />
       <HeroSection />
       <PackageHighlights />
+      <ServicesOverview />
       <footer className="mt-auto py-4">
         {/* Footer content */}
       </footer>
