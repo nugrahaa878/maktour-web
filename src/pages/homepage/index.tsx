bb1d6da3eb5eee076a41410173233bcface206b5
Ari Nugraha <nugrahaa878@gmail.com>
2025-02-18 16:51:16 +0700

    add package

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,11 +1,13 @@
 import Navbar from '../../components/navbar';
 import HeroSection from '../../components/hero-section';
+import PackageHighlights from 'components/package-highlights';
 
 function HomePage() {
   return (
     <main className="min-h-screen">
       <Navbar />
       <HeroSection />
+      <PackageHighlights />
       <footer className="mt-auto py-4">
         {/* Footer content */}
       </footer>
